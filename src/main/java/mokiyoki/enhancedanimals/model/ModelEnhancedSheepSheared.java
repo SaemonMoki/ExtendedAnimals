e654c7d283332ed19333c11ba97bf9df57de15c8
Saemon <saemonkitsune@gmail.com>
2019-02-11 21:49:46 +1000

    added back the animation set's of sheep

--- src/main/java/mokiyoki/enhancedanimals/model/ModelEnhancedSheepSheared.java
+++ src/main/java/mokiyoki/enhancedanimals/model/ModelEnhancedSheepSheared.java
@@ -5,6 +5,7 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.MathHelper;
 
 public class ModelEnhancedSheepSheared extends ModelBase {
 
@@ -48,6 +49,8 @@ public class ModelEnhancedSheepSheared extends ModelBase {
     }
 
     public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
+
         this.head.render(scale);
         this.body.render(scale);
         this.tail.render(scale);
@@ -65,6 +68,13 @@ public class ModelEnhancedSheepSheared extends ModelBase {
     public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
     {
         super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityIn);
+        this.head.rotateAngleX = headPitch * 0.017453292F;
+        this.head.rotateAngleY = netHeadYaw * 0.017453292F;
+//        this.body.rotateAngleX = ((float)Math.PI / 2F);
+        this.leg1.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.leg2.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.leg3.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.leg4.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
         this.head.rotateAngleX = 1.2F + this.headRotationAngleX;
     }
 

2d31fa6c020f13d035cf6384c83f71539e98d1b3
Saemon <saemonkitsune@gmail.com>
2019-03-16 20:00:33 +1000

    fixed chicken egg capabilities and spawning from eggs

--- src/main/java/mokiyoki/enhancedanimals/entity/EnhancedEntityEgg.java
+++ src/main/java/mokiyoki/enhancedanimals/entity/EnhancedEntityEgg.java
@@ -1,20 +1,32 @@
 package mokiyoki.enhancedanimals.entity;
 
+import mokiyoki.enhancedanimals.capability.egg.EggCapabilityProvider;
+import mokiyoki.enhancedanimals.capability.egg.IEggCapability;
 import mokiyoki.enhancedanimals.init.ModItems;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.init.Particles;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.particles.ItemParticleData;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 import static mokiyoki.enhancedanimals.util.handlers.RegistryHandler.ENHANCED_ENTITY_EGG_ENTITY_TYPE;
 
 public class EnhancedEntityEgg extends EntityThrowable {
+
+    private static final DataParameter<String> GENES = EntityDataManager.<String>createKey(EnhancedEntityEgg.class, DataSerializers.STRING);
+
     public EnhancedEntityEgg(World worldIn) {
         super(ENHANCED_ENTITY_EGG_ENTITY_TYPE, worldIn);
     }
@@ -25,6 +37,31 @@ public class EnhancedEntityEgg extends EntityThrowable {
 
     public EnhancedEntityEgg(World worldIn, double x, double y, double z) {
         super(ENHANCED_ENTITY_EGG_ENTITY_TYPE, x, y, z,worldIn);
+    }
+
+    public EnhancedEntityEgg(World worldIn, EntityPlayer playerIn, int[] eggGenes) {
+        super(ENHANCED_ENTITY_EGG_ENTITY_TYPE, playerIn, worldIn);
+        this.setGenes(eggGenes);
+    }
+
+    protected void registerData() {
+        this.getDataManager().register(GENES, new String());
+    }
+
+    public void setGenes(int[] eggGenes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < eggGenes.length; i++){
+            sb.append(eggGenes[i]);
+            if (i != eggGenes.length -1){
+                sb.append(",");
+            }
+        }
+        this.getDataManager().set(GENES, sb.toString());
+    }
+
+    public String getGenes() {
+        String genes = ((String)this.dataManager.get(GENES)).toString();
+        return genes;
     }
 
     /**
@@ -58,7 +95,8 @@ public class EnhancedEntityEgg extends EntityThrowable {
 
                 for (int j = 0; j < i; ++j) {
                     EnhancedChicken enhancedchicken = new EnhancedChicken(this.world);
-//                    enhancedchicken.setSharedGenes(eggGenes);
+                    enhancedchicken.setSharedGenesFromEntityEgg(getGenes());
+                    enhancedchicken.setGenes(enhancedchicken.getSharedGenes());
                     enhancedchicken.setGrowingAge(-24000);
                     enhancedchicken.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
                     this.world.spawnEntity(enhancedchicken);
@@ -69,4 +107,24 @@ public class EnhancedEntityEgg extends EntityThrowable {
             this.remove();
         }
     }
+
+    @Override
+    public void readAdditional(NBTTagCompound compound) {
+        super.readAdditional(compound);
+        String genes = compound.getString("genes");
+        this.getDataManager().set(GENES, genes);
+
+    }
+
+    @Override
+    public void writeAdditional(NBTTagCompound compound) {
+        super.writeAdditional(compound);
+        String genes = this.getGenes();
+        if (!genes.isEmpty()) {
+            compound.setString("genes", genes);
+        }
+
+    }
+
+
 }

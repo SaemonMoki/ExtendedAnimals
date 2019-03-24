590b423270c8b356a3d170ee28a4fdefc40e4895
Saemon <saemonkitsune@gmail.com>
2019-03-24 18:47:46 +1000

    fixed animations with rabbits back legs

--- src/main/java/mokiyoki/enhancedanimals/model/ModelEnhancedRabbit.java
+++ src/main/java/mokiyoki/enhancedanimals/model/ModelEnhancedRabbit.java
@@ -1,10 +1,12 @@
 package mokiyoki.enhancedanimals.model;
 
+import mokiyoki.enhancedanimals.entity.EnhancedRabbit;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.model.ModelBase;
 import net.minecraft.client.renderer.entity.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -30,7 +32,7 @@ public class ModelEnhancedRabbit extends ModelBase
     private final ModelRenderer rabbitLeftEar;
     private final ModelRenderer rabbitTail;
     private final ModelRenderer rabbitNose;
-//    private float jumpRotation;
+    private float jumpRotation;
 
     public ModelEnhancedRabbit()
     {
@@ -39,27 +41,37 @@ public class ModelEnhancedRabbit extends ModelBase
 
         this.rabbitLeftFoot = new ModelRenderer(this, 0, 55);
         this.rabbitLeftFoot.addBox(0F, 0F, 0F, 3, 8, 1);
-        this.rabbitLeftFoot.setRotationPoint(-5.0F, 0F, 0F);
+        this.rabbitLeftFoot.setRotationPoint(0.0F, 5.0F, 1.0F);
+        this.setRotationOffset(this.rabbitLeftFoot, 3.0F, 0.0F, 0.0F);
+        this.rabbitLeftFoot.mirror = true;
 
         this.rabbitRightFoot = new ModelRenderer(this, 8, 55);
         this.rabbitRightFoot.addBox(0F, 0F, 0F, 3, 8, 1);
-        this.rabbitRightFoot.setRotationPoint(0F, 0F, 0F);
+        this.rabbitRightFoot.setRotationPoint(0F, 5.0F, 1.0F);
+        this.setRotationOffset(this.rabbitRightFoot, 3.0F, 0.0F, 0.0F);
+        this.rabbitRightFoot.mirror = true;
 
         this.rabbitLeftCalf = new ModelRenderer(this, 0, 49);
         this.rabbitLeftCalf.addBox(0F, 0F, 0F, 3, 5, 1);
-        this.rabbitLeftCalf.setRotationPoint(-5.0F, 0F, 0F);
+        this.rabbitLeftCalf.setRotationPoint(0.0F, 6.5F, 2.7F);
+        this.setRotationOffset(this.rabbitLeftCalf, 2.0F, 0.0F, 0.0F);
+        this.rabbitLeftCalf.addChild(rabbitLeftFoot);
 
         this.rabbitRightCalf = new ModelRenderer(this, 18, 49);
         this.rabbitRightCalf.addBox(0F, 0F, 0F, 3, 5, 1);
-        this.rabbitRightCalf.setRotationPoint(0F, 0F, 0F);
+        this.rabbitRightCalf.setRotationPoint(0, 6.5F, 2.7F);
+        this.setRotationOffset(this.rabbitRightCalf, 2.0F, 0.0F, 0.0F);
+        this.rabbitRightCalf.addChild(rabbitRightFoot);
 
         this.rabbitLeftThigh = new ModelRenderer(this, 0, 37);
         this.rabbitLeftThigh.addBox(0F, 0F, 0F, 3, 6, 6);
         this.rabbitLeftThigh.setRotationPoint(-4.5F, 17F, 4F);
+        this.rabbitLeftThigh.addChild(rabbitLeftCalf);
 
         this.rabbitRightThigh = new ModelRenderer(this, 18, 37);
         this.rabbitRightThigh.addBox(0F, 0F, 0F, 3, 6, 6);
         this.rabbitRightThigh.setRotationPoint(1.5F, 17F, 4F);
+        this.rabbitRightThigh.addChild(rabbitRightCalf);
 
         this.rabbitBody = new ModelRenderer(this, 7, 8);
         this.rabbitBody.addBox(-3.5F, 0F, 0F, 7, 7, 9);
@@ -114,15 +126,19 @@ public class ModelEnhancedRabbit extends ModelBase
         this.rabbitTail.setRotationPoint(0.0F, 0.0F, 0.0F);
     }
 
+    private void setRotationOffset(ModelRenderer renderer, float x, float y, float z) {
+        renderer.rotateAngleX = x;
+        renderer.rotateAngleY = y;
+        renderer.rotateAngleZ = z;
+    }
+
     /**
      * Sets the models various rotation angles then renders the model.
      */
-    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale)
-    {
+    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
         this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
 
-        if (this.isChild)
-        {
+        if (this.isChild) {
 //            float f = 1.5F;
             GlStateManager.pushMatrix();
             GlStateManager.scalef(0.56666666F, 0.56666666F, 0.56666666F);
@@ -137,10 +153,10 @@ public class ModelEnhancedRabbit extends ModelBase
             GlStateManager.pushMatrix();
             GlStateManager.scalef(0.4F, 0.4F, 0.4F);
             GlStateManager.translatef(0.0F, 36.0F * scale, 0.0F);
-            this.rabbitLeftFoot.render(scale);
-            this.rabbitRightFoot.render(scale);
-            this.rabbitLeftCalf.render(scale);
-            this.rabbitRightCalf.render(scale);
+//            this.rabbitLeftFoot.render(scale);
+//            this.rabbitRightFoot.render(scale);
+//            this.rabbitLeftCalf.render(scale);
+//            this.rabbitRightCalf.render(scale);
             this.rabbitLeftThigh.render(scale);
             this.rabbitRightThigh.render(scale);
             this.rabbitBody.render(scale);
@@ -151,9 +167,7 @@ public class ModelEnhancedRabbit extends ModelBase
             this.rabbitRightArm.render(scale);
             this.rabbitTail.render(scale);
             GlStateManager.popMatrix();
-        }
-        else
-        {
+        } else {
 //            GlStateManager.pushMatrix();
 //            GlStateManager.scalef(0.6F, 0.6F, 0.6F);
 //            GlStateManager.translatef(0.0F, 16.0F * scale, 0.0F);
@@ -170,7 +184,7 @@ public class ModelEnhancedRabbit extends ModelBase
             this.rabbitLeftThigh.render(scale);
             this.rabbitRightThigh.render(scale);
             this.rabbitBody.render(scale);
-////            this.rabbitButtRound.render(scale);
+//            this.rabbitButtRound.render(scale);
             this.rabbitButt.render(scale);
 ////            this.rabbitButtTube.render(scale);
             this.rabbitLeftArm.render(scale);
@@ -185,9 +199,8 @@ public class ModelEnhancedRabbit extends ModelBase
      * and legs, where par1 represents the time(so that arms and legs swing back and forth) and par2 represents how
      * "far" arms and legs can swing at most.
      */
-    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
-    {
-//        float f = ageInTicks - (float)entityIn.ticksExisted;
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
+        float f = ageInTicks - (float)entityIn.ticksExisted;
         this.rabbitHeadLeft.rotateAngleX = headPitch * 0.017453292F;
         this.rabbitHeadLeft.rotateAngleY = netHeadYaw * 0.017453292F;
         copyModelAngles(rabbitHeadLeft, rabbitHeadRight);
@@ -195,11 +208,28 @@ public class ModelEnhancedRabbit extends ModelBase
         copyModelAngles(rabbitHeadLeft, rabbitNose);
         copyModelAngles(rabbitHeadLeft, rabbitLeftEar);
         copyModelAngles(rabbitHeadLeft, rabbitRightEar);
-//        this.rabbitNose.rotateAngleY = netHeadYaw * 0.017453292F;
-//        this.rabbitRightEar.rotateAngleY = this.rabbitNose.rotateAngleY - 0.2617994F;
-//        this.rabbitLeftEar.rotateAngleY = this.rabbitNose.rotateAngleY + 0.2617994F;
-//        this.rabbitLeftFoot.rotateAngleX = this.jumpRotation * 50.0F * 0.017453292F;
-//        this.rabbitRightFoot.rotateAngleX = this.jumpRotation * 50.0F * 0.017453292F;
+        this.rabbitNose.rotateAngleY = netHeadYaw * 0.017453292F;
+        this.rabbitRightEar.rotateAngleY = this.rabbitNose.rotateAngleY - 0.2617994F;
+        this.rabbitLeftEar.rotateAngleY = this.rabbitNose.rotateAngleY + 0.2617994F;
+
+        this.jumpRotation = MathHelper.sin(((EnhancedRabbit)entityIn).getJumpCompletion(f) * (float)Math.PI);
+        if (this.jumpRotation == 0.0F) {
+            this.rabbitLeftFoot.rotateAngleX = 3.0F;
+            this.rabbitRightFoot.rotateAngleX = 3.0F;
+            this.rabbitLeftCalf.rotateAngleX = 2.0F;
+            this.rabbitRightCalf.rotateAngleX = 2.0F;
+        } else {
+            this.rabbitLeftFoot.rotateAngleX = 3.0F + this.jumpRotation * 80.0F * ((float)Math.PI / 180F);
+            this.rabbitRightFoot.rotateAngleX = 3.0F + this.jumpRotation * 80.0F * ((float)Math.PI / 180F);
+            this.rabbitLeftCalf.rotateAngleX = 2.0F - (this.jumpRotation * +50.0F * ((float)Math.PI / 180F));
+            this.rabbitRightCalf.rotateAngleX = 2.0F - (this.jumpRotation * +50.0F * ((float)Math.PI / 180F));
+        }
+        this.rabbitLeftThigh.rotateAngleX = (this.jumpRotation * 50.0F - 21.0F) * ((float)Math.PI / 180F);
+        this.rabbitRightThigh.rotateAngleX = (this.jumpRotation * 50.0F - 21.0F) * ((float)Math.PI / 180F);
+        this.rabbitLeftArm.rotateAngleX = (this.jumpRotation * -40.0F - 11.0F) * ((float)Math.PI / 180F);
+        this.rabbitRightArm.rotateAngleX = (this.jumpRotation * -40.0F - 11.0F) * ((float)Math.PI / 180F);
+
+
         copyModelAngles(rabbitButt, rabbitTail);
     }
 
@@ -207,9 +237,8 @@ public class ModelEnhancedRabbit extends ModelBase
      * Used for easily adding entity-dependent animations. The second and third float params here are the same second
      * and third as in the setRotationAngles method.
      */
-    public void setLivingAnimations(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTickTime)
-    {
-//        super.setLivingAnimations(entitylivingbaseIn, limbSwing, limbSwingAmount, partialTickTime);
-//        this.jumpRotation = MathHelper.sin(((EntityRabbit)entitylivingbaseIn).setJumpCompletion(partialTickTime) * (float)Math.PI);
+    public void setLivingAnimations(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTickTime) {
+        super.setLivingAnimations(entitylivingbaseIn, limbSwing, limbSwingAmount, partialTickTime);
+        this.jumpRotation = MathHelper.sin(((EnhancedRabbit)entitylivingbaseIn).getJumpCompletion(partialTickTime) * (float)Math.PI);
     }
 }

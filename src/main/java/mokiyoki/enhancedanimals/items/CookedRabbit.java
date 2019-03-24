65fbef977d5387185cab1ead199845777d5c7b37
MokiYoki <saemonkitsune@gmail.com>
2019-03-25 04:52:31 +1000

    added rabbit size meat items added rabbit loot table added llama loot table something is wrong with spawing rates, 0 to 1 animals spawn where there should be a group

---
+++ src/main/java/mokiyoki/enhancedanimals/items/CookedRabbit.java
@@ -0,0 +1,11 @@
+package mokiyoki.enhancedanimals.items;
+
+import net.minecraft.item.ItemFood;
+
+public class CookedRabbit extends ItemFood {
+
+    public CookedRabbit(Properties props, int amount, float saturation, boolean isMeat) {
+        super(amount, saturation, isMeat, props);
+    }
+
+}
---
+++ src/main/java/mokiyoki/enhancedanimals/items/RawRabbit.java
@@ -0,0 +1,10 @@
+package mokiyoki.enhancedanimals.items;
+
+import net.minecraft.item.ItemFood;
+
+public class RawRabbit extends ItemFood {
+
+    public RawRabbit(Properties props, int amount, float saturation, boolean isMeat) {
+        super(amount, saturation, isMeat, props);
+    }
+}

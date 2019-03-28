173ed7ed581eb19f28ab43834b3ca9d8fb3edfb4
MokiYoki <saemonkitsune@gmail.com>
2019-03-28 15:58:12 +1000

    added rabbit drops added llama drops rewrote rabbit texture logic bug fixed chicken textures bug fixed rabbit broken/charlie texture selection bug fixed post crafting

--- src/main/java/mokiyoki/enhancedanimals/items/RabbitStew.java
+++ src/main/java/mokiyoki/enhancedanimals/items/RabbitStew.java
@@ -1,11 +1,20 @@
 package mokiyoki.enhancedanimals.items;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class RabbitStew extends ItemFood {
 
     public RabbitStew(Properties props, int amount, float saturation, boolean isMeat) {
         super(amount, saturation, isMeat, props);
+    }
+
+    @Override
+    protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
+
     }
 
 }

1837345d739ccb2c7f1fe4b1e817e6d33a80c8ab
Saemon <saemonkitsune@gmail.com>
2019-04-08 22:17:19 +1000

    fixed capabilities with other mods

--- src/main/java/mokiyoki/enhancedanimals/capability/post/PostCapabilityProvider.java
+++ src/main/java/mokiyoki/enhancedanimals/capability/post/PostCapabilityProvider.java
@@ -2,6 +2,7 @@ package mokiyoki.enhancedanimals.capability.post;
 
 import net.minecraft.nbt.INBTBase;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
@@ -9,38 +10,55 @@ import net.minecraftforge.common.util.LazyOptional;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by saemon on 29/09/2018.
  */
-public class PostCapabilityProvider implements ICapabilitySerializable<INBTBase> {
+public class PostCapabilityProvider implements IPostCapability, ICapabilitySerializable<INBTBase> {
 
     @CapabilityInject(IPostCapability.class)
     public static final Capability<IPostCapability> POST_CAP = null;
 
-    private IPostCapability instance = POST_CAP.getDefaultInstance();
+    private final LazyOptional<IPostCapability> holder = LazyOptional.of(() -> this);
 
-    private final LazyOptional<IPostCapability> holder = LazyOptional.of(() -> instance);
+    List<BlockPos> postBlockPositions = new ArrayList<>();
 
-//    @Override
-//    public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing) {
-//        return capability == POST_CAP;
-//    }
+    @Override
+    public List<BlockPos> getAllPostPos() {
+        return postBlockPositions;
+    }
+
+    @Override
+    public void addPostPos(BlockPos blockPos) {
+        postBlockPositions.add(blockPos);
+    }
+
+    @Override
+    public void removePostPos(BlockPos blockPos) {
+        postBlockPositions.remove(blockPos);
+    }
+
+    @Override
+    public void setAllPostPos(List<BlockPos> blockPosList) {
+        this.postBlockPositions = blockPosList;
+    }
 
     @Nullable
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
-        return capability == POST_CAP ? holder.cast() : null;
+        return POST_CAP.orEmpty(capability, holder);
     }
 
 
     @Override
     public INBTBase serializeNBT() {
-        return POST_CAP.getStorage().writeNBT(POST_CAP, this.instance, null);
+        return POST_CAP.getStorage().writeNBT(POST_CAP, this, null);
     }
 
     @Override
     public void deserializeNBT(INBTBase nbt) {
-        POST_CAP.getStorage().readNBT(POST_CAP, this.instance, null, nbt);
+        POST_CAP.getStorage().readNBT(POST_CAP, this, null, nbt);
     }
 }

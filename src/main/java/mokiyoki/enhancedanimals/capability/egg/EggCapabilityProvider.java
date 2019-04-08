f7fd0d636df2f34854de24df95c2b841f5748de6
Saemon <saemonkitsune@gmail.com>
2019-04-08 22:17:19 +1000

    fixed capabilities with other mods

--- src/main/java/mokiyoki/enhancedanimals/capability/egg/EggCapabilityProvider.java
+++ src/main/java/mokiyoki/enhancedanimals/capability/egg/EggCapabilityProvider.java
@@ -13,28 +13,39 @@ import javax.annotation.Nullable;
 /**
  * Created by saemon on 30/09/2018.
  */
-public class EggCapabilityProvider implements ICapabilitySerializable<INBTBase> {
+public class EggCapabilityProvider implements IEggCapability, ICapabilitySerializable<INBTBase> {
 
     @CapabilityInject(IEggCapability.class)
     public static final Capability<IEggCapability> EGG_CAP = null;
 
-    private IEggCapability instance = EGG_CAP.getDefaultInstance();
+//    private IEggCapability instance = EGG_CAP.getDefaultInstance();
 
-    private final LazyOptional<IEggCapability> holder = LazyOptional.of(() -> instance);
+    private final LazyOptional<IEggCapability> holder = LazyOptional.of(() -> this);
+
+    private int[] genes;
+    @Override
+    public int[] getGenes() {
+        return this.genes;
+    }
+
+    @Override
+    public void setGenes(int[] genes) {
+        this.genes = genes;
+    }
 
     @Nullable
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
-        return capability == EGG_CAP ? holder.cast() : null;
+        return EGG_CAP.orEmpty(capability, holder);
     }
 
     @Override
     public INBTBase serializeNBT() {
-        return EGG_CAP.getStorage().writeNBT(EGG_CAP, this.instance, null);
+        return EGG_CAP.getStorage().writeNBT(EGG_CAP, this, null);
     }
 
     @Override
     public void deserializeNBT(INBTBase nbt) {
-        EGG_CAP.getStorage().readNBT(EGG_CAP, this.instance, null, nbt);
+        EGG_CAP.getStorage().readNBT(EGG_CAP, this, null, nbt);
     }
 }

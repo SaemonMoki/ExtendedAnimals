cd61ebb2988fa611e6726a3e75eda82a1cbd824f
Saemon <saemonkitsune@gmail.com>
2019-08-25 12:40:06 +1000

    fixed mooshroom mushroom layer rendering

--- src/main/java/mokiyoki/enhancedanimals/renderer/layers/EnhancedMooshroomMushroomLayer.java
+++ src/main/java/mokiyoki/enhancedanimals/renderer/layers/EnhancedMooshroomMushroomLayer.java
@@ -5,8 +5,6 @@ package mokiyoki.enhancedanimals.renderer.layers;
 // (powered by Fernflower decompiler)
 //
 
-package net.minecraft.client.renderer.entity.layers;
-
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.platform.GlStateManager.CullFace;
 import net.minecraft.block.BlockState;
@@ -26,37 +24,37 @@ public class EnhancedMooshroomMushroomLayer<T extends EnhancedMooshroom> extends
         super(p_i50931_1_);
     }
 
-    public void render(T p_212842_1_, float p_212842_2_, float p_212842_3_, float p_212842_4_, float p_212842_5_, float p_212842_6_, float p_212842_7_, float p_212842_8_) {
-        if (!p_212842_1_.isChild() && !p_212842_1_.isInvisible()) {
-            BlockState lvt_9_1_ = p_212842_1_.getMooshroomType().getRenderState();
+    public void render(T entityIn, float p_212842_2_, float p_212842_3_, float p_212842_4_, float p_212842_5_, float p_212842_6_, float p_212842_7_, float p_212842_8_) {
+        if (!entityIn.isChild() && !entityIn.isInvisible()) {
+            BlockState blockstate = entityIn.getMooshroomType().getRenderState();
             this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
             GlStateManager.enableCull();
-            GlStateManager.cullFace(CullFace.FRONT);
+            GlStateManager.cullFace(GlStateManager.CullFace.FRONT);
             GlStateManager.pushMatrix();
             GlStateManager.scalef(1.0F, -1.0F, 1.0F);
             GlStateManager.translatef(0.2F, 0.35F, 0.5F);
             GlStateManager.rotatef(42.0F, 0.0F, 1.0F, 0.0F);
-            BlockRendererDispatcher lvt_10_1_ = Minecraft.getInstance().getBlockRendererDispatcher();
+            BlockRendererDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
             GlStateManager.pushMatrix();
             GlStateManager.translatef(-0.5F, -0.5F, 0.5F);
-            lvt_10_1_.renderBlockBrightness(lvt_9_1_, 1.0F);
+            blockrendererdispatcher.renderBlockBrightness(blockstate, 1.0F);
             GlStateManager.popMatrix();
             GlStateManager.pushMatrix();
             GlStateManager.translatef(0.1F, 0.0F, -0.6F);
             GlStateManager.rotatef(42.0F, 0.0F, 1.0F, 0.0F);
             GlStateManager.translatef(-0.5F, -0.5F, 0.5F);
-            lvt_10_1_.renderBlockBrightness(lvt_9_1_, 1.0F);
+            blockrendererdispatcher.renderBlockBrightness(blockstate, 1.0F);
             GlStateManager.popMatrix();
             GlStateManager.popMatrix();
             GlStateManager.pushMatrix();
-            ((ModelEnhancedCow)this.getEntityModel()).getHead().postRender(0.0625F);
+            this.getEntityModel().getHead().postRender(0.0625F);
             GlStateManager.scalef(1.0F, -1.0F, 1.0F);
             GlStateManager.translatef(0.0F, 0.7F, -0.2F);
             GlStateManager.rotatef(12.0F, 0.0F, 1.0F, 0.0F);
             GlStateManager.translatef(-0.5F, -0.5F, 0.5F);
-            lvt_10_1_.renderBlockBrightness(lvt_9_1_, 1.0F);
+            blockrendererdispatcher.renderBlockBrightness(blockstate, 1.0F);
             GlStateManager.popMatrix();
-            GlStateManager.cullFace(CullFace.BACK);
+            GlStateManager.cullFace(GlStateManager.CullFace.BACK);
             GlStateManager.disableCull();
         }
     }

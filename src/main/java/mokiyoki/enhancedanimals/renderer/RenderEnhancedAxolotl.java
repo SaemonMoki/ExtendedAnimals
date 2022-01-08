2fc79ee721d19b94e96d4a9bfc118c5857c3c7ea
Saemon <saemonkitsune@gmail.com>
2022-01-08 18:12:25 +1000

    updated axolotl model and render

--- src/main/java/mokiyoki/enhancedanimals/renderer/RenderEnhancedAxolotl.java
+++ src/main/java/mokiyoki/enhancedanimals/renderer/RenderEnhancedAxolotl.java
@@ -13,7 +13,10 @@ import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public class RenderEnhancedAxolotl extends MobRenderer<EnhancedAxolotl, ModelEnhancedAxolotl<EnhancedAxolotl>> {
     private static final LayeredTextureCacher textureCache = new LayeredTextureCacher();
     private static final String ENHANCED_AXOLOTL_TEXTURE_LOCATION = "eanimod:textures/entities/axolotl/";

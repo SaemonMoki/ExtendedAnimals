3713ec5ce2f2c14d0b39bd18cd1c1d1880cbf64b
SeamusMoffatMaia <smoffat@maiatechnology.com.au>
2023-01-29 14:42:45 +1000

    Fixed debug book not displaying an informative tooltip fixed mooshroom's not grazing

--- src/main/java/mokiyoki/enhancedanimals/items/DebugGenesBook.java
+++ src/main/java/mokiyoki/enhancedanimals/items/DebugGenesBook.java
@@ -1,11 +1,19 @@
 package mokiyoki.enhancedanimals.items;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 
 public class DebugGenesBook extends Item {
@@ -21,5 +29,10 @@ public class DebugGenesBook extends Item {
      */
     public InteractionResult interactLivingEntity(ItemStack stack, Player playerIn, LivingEntity target, InteractionHand hand) {
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> components, TooltipFlag p_41424_) {
+        components.add(new TranslatableComponent("item.eanimod.debug_gene_book_hover").withStyle(ChatFormatting.GRAY));
     }
 }

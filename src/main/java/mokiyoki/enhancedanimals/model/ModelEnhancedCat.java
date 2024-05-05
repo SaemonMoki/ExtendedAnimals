ebe9ffb106f4eca30795f3a64e1181181b8b64eb
SaemonMoki <43024803+SaemonMoki@users.noreply.github.com>
2024-05-06 00:10:00 +1000

    Fixed eye hyper and hypo dilation issues. Also increased the max pupil size.

--- src/main/java/mokiyoki/enhancedanimals/model/ModelEnhancedCat.java
+++ src/main/java/mokiyoki/enhancedanimals/model/ModelEnhancedCat.java
@@ -689,9 +689,13 @@ public class ModelEnhancedCat<T extends EnhancedCat> extends EnhancedAnimalModel
             if (false /*TODO this makes the cat partially close its eyes for stuff like slow blink*/) {
                 mapOfScale.put("eyes", ModelHelper.createScalings(1.0F, 0.5F/*TODO eye openness percentage*/, 1.0F, 0.0F, 0.0F, 0.0F));
             }
-            if (dilation!=1.0F /*TODO this controls pupil dilation*/) {
-                mapOfScale.put("eyeL2", ModelHelper.createScalings(2f*dilation, 1.0F, 1.0F, (1.0F-dilation)*-0.065F, 0.0F, 0.0F));
-                mapOfScale.put("eyeR2", ModelHelper.createScalings(2f*dilation, 1.0F, 1.0F, (1.0F-dilation)*0.065F, 0.0F, 0.0F));
+            dilation*=2.0F;
+            if (dilation!=1.0F) {
+                dilation = Math.max(dilation, 0.05F);
+                dilation = Math.min(dilation, 1.5F);
+                float xOff = (1.0F-dilation)*0.065F;
+                mapOfScale.put("eyeL2", ModelHelper.createScalings(dilation, 1.0F, 1.0F, dilation>1.0F? 0.5F*xOff : -xOff, 0.0F, 0.0F));
+                mapOfScale.put("eyeR2", ModelHelper.createScalings(dilation, 1.0F, 1.0F,  dilation>1.0F? -0.5F*xOff : xOff, 0.0F, 0.0F));
             }
 
             this.bodyFurFrontShort.show(cat.furnishings == 1);

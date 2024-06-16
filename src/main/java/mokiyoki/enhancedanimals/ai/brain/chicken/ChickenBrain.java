2a6d3c7338bbeb725a3f8f4c22343d1a0b22f5a4
beardeddev <saemonkitsune@gmail.com>
2024-06-16 14:32:10 +1000

    Fixed RunIf issues in brain Fixed egg timer issues for first egg and restarts

--- src/main/java/mokiyoki/enhancedanimals/ai/brain/chicken/ChickenBrain.java
+++ src/main/java/mokiyoki/enhancedanimals/ai/brain/chicken/ChickenBrain.java
@@ -102,10 +102,10 @@ public class ChickenBrain {
         brain.addActivity(Activity.IDLE, ImmutableList.of(
                 Pair.of(0, new RunSometimes<>(new RunIf<>(ChickenBrain::canMoveOrLookAround, new SetEntityLookTarget(EntityType.PLAYER, 6.0F)), UniformInt.of(100, 600))),
                 Pair.of(1, new RunSometimes<>(new RunIf<>(ChickenBrain::canMoveOrLookAround, new SetEntityLookTarget(ModEntities.ENHANCED_CHICKEN.get(), 6.0F)), UniformInt.of(50, 200))),
-                Pair.of(1, new RunIf<>(ChickenBrain::notBroody, new ChickenMakeLove(1.0F))),
+                Pair.of(1, new RunIf<>(ChickenBrain::notBroody, new ChickenMakeLove(1.0F), true)),
                 Pair.of(2, new SeekingNest()),
                 Pair.of(3, new RunOne<>(ImmutableList.of(
-                        Pair.of(new RunIf<>(ChickenBrain::canMoveOrLookAround, new FollowTemptation(ChickenBrain::getSpeedModifier)), 1),
+                        Pair.of(new RunIf<>(ChickenBrain::canMoveOrLookAround, new FollowTemptation(ChickenBrain::getSpeedModifier), true), 1),
                         Pair.of(new BabyFollowParent<>(ADULT_FOLLOW_RANGE, ChickenBrain::getSpeedModifierFollowingAdult), 1)))
                 ),
                 Pair.of(4, new StartAttacking<>(ChickenBrain::findNearestValidAttackTarget)),

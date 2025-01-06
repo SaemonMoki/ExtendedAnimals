5b2e7247e9ab0a53805aa5ce4113b596c875c8e9
beardeddev <saemonkitsune@gmail.com>
2025-01-06 18:55:25 +1000

    Fixed issue with mottle in pattern check for chicks causing an empty pattern texture grouping

--- src/main/java/mokiyoki/enhancedanimals/renderer/textures/ChickenTexture.java
+++ src/main/java/mokiyoki/enhancedanimals/renderer/textures/ChickenTexture.java
@@ -72,7 +72,7 @@ public class ChickenTexture {
                 featherGroup.addGrouping(baseFeatherColour);
 
                 if (gene[20] == 1 || gene[21] == 1) {
-                    if (!pattern.isEmpty() || mottled) {
+                    if (!pattern.isEmpty()) {
                         TextureGrouping patternFeatherGroup = new TextureGrouping(TexturingType.MASK_GROUP);
                         TextureGrouping patternCutOutGroup = new TextureGrouping(TexturingType.MERGE_GROUP);
                         chicken.addTextureToAnimalTextureGrouping(patternCutOutGroup, "chick/pattern/" + pattern + ".png", "chick"+pattern);

3c7065e7d2e58270f341532217ada83b94cc6178
moki <geneticanimals.mokifox@gmail.com>
2026-04-29 08:23:25 +1000

    added rough version of mod tags

---
+++ src/main/java/mokiyoki/enhancedanimals/init/ModTags.java
@@ -0,0 +1,33 @@
+package mokiyoki.enhancedanimals.init;
+
+import mokiyoki.enhancedanimals.util.Reference;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> GRAZEABLE_BLOCKS = tag("grazeable_blocks");
+        public static final TagKey<Block> CONSUMABLE_BLOCKS = tag("consumable_blocks");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(Reference.MODID, name));
+        }
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Items {
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(Reference.MODID, name));
+        }
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+}

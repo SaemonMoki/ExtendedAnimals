1a888996887ddfeef55cbddfe2e66ce4fcd25a4c
moki <geneticanimals.mokifox@gmail.com>
2026-05-12 10:02:29 +1000

    added some more spots

--- src/main/java/mokiyoki/enhancedanimals/renderer/textures/AxolotlEggTexture.java
+++ src/main/java/mokiyoki/enhancedanimals/renderer/textures/AxolotlEggTexture.java
@@ -4,6 +4,8 @@ import mokiyoki.enhancedanimals.entity.EnhancedAxolotlEgg;
 import mokiyoki.enhancedanimals.renderer.texture.TextureGrouping;
 import mokiyoki.enhancedanimals.renderer.texture.TexturingType;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class AxolotlEggTexture {
     private String delimiter = "";
 
@@ -15,9 +17,13 @@ public class AxolotlEggTexture {
         if (gene(g, 12)>1 && gene(g, 13)>1) {
             //pied
             int piedStrength = (int) ((gene(g, 14) + gene(g, 15) - 2) * 0.3F);
+            int r = ThreadLocalRandom.current().nextInt(3);
+            egg.addTexturetoTextureGroup(parentGroup, "piebald/"+r+".png", String.valueOf(r));
+        } else {
+            egg.addDelimiter();
         }
 
-        egg.addTextureToAnimal("shell.png", String.valueOf(0));
+        egg.addTexturetoTextureGroup(parentGroup, "shell.png", String.valueOf(0));
 
         egg.setTextureGrouping(parentGroup);
     }
@@ -27,28 +33,32 @@ public class AxolotlEggTexture {
     }
 
     private static String baseColourMutation(String[] g) {
-        if (gene(g, 0)+ gene(g, 1)==4) {
-            //albino
-            if (gene(g, 2)+ gene(g, 3)==4) {
-                return "albino";
-            } else {
-                return "gold";
-            }
+        if ((gene(g, 8)+ gene(g, 9)==4)) {
+            return "white";
         } else {
-            if (gene(g, 2)+ gene(g, 3)==4) {
-                //axanthic
-                if (gene(g, 6)+ gene(g, 7)==4) {
-                    //lavender
-                    return "lavender";
+            if (gene(g, 0) + gene(g, 1) == 4) {
+                //albino
+                if (gene(g, 2) + gene(g, 3) == 4) {
+                    return "white";
                 } else {
-                    return "axanthic";
+                    return "gold";
                 }
             } else {
-                if (gene(g, 6)+ gene(g, 7)==4) {
-                    //copper
-                    return "copper";
+                if (gene(g, 2) + gene(g, 3) == 4) {
+                    //axanthic
+                    if (gene(g, 6) + gene(g, 7) == 4) {
+                        //lavender
+                        return "lavender";
+                    } else {
+                        return "axanthic";
+                    }
                 } else {
-                    return "wildtype";
+                    if (gene(g, 6) + gene(g, 7) == 4) {
+                        //copper
+                        return "copper";
+                    } else {
+                        return "wildtype";
+                    }
                 }
             }
         }
